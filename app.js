47e80b8237e410fedbafa508f361cadb990b0472
GMDEEP <stewartjgreen@gmail.com>
2021-07-16 00:46:27 +0000

    final Update

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
-// one euro is:
 let oneEuroIs = {
     "JPY": 127.9, // japan yen
     "USD": 1.2, // us dollar
     "GBP": 0.8, // british pound
 }
-
-// we declare the function with the exact name "fromEuroToDollar"
 const fromEuroToDollar = function(valueInEuro){
     // convert the given valueInEuro to dollars
-    let valueInDollar = valueInEuro * 1.2;
+    let valueInDollar = valueInEuro * oneEuroIs["USD"];
     //return the dollar value
     return valueInDollar;
 }
-
-const fromDollarToYen = function(valueInEuro){
-    // convert the given valueInEuro to dollars
-    let valueInYen = valueInEuro * 127.9;
-    //return the dollar value
+// we declare the function with the exact name "fromDollarToYen"
+const fromDollarToYen = function(valueInDollar){
+    // convert the valueInDollar back to euro and then converting to yen
+    let valueInYen = (valueInDollar / oneEuroIs["USD"]) * oneEuroIs["JPY"];
+    //return the yen value
     return valueInYen;
 }
-
-const fromYenToPound = function(valueInEuro){
-    // convert the given valueInEuro to dollars
-    let valueInEuro = valueInEuro * 0.8;
-    //return the dollar value
+// we declare the function with the exact name "fromYenToPound"
+const fromYenToPound = function(valueInYen){
+    // convert the valueInYen to pound
+    let valueInPound = (valueInYen / oneEuroIs["JPY"]) * oneEuroIs["GBP"];
+    //return the pound value
     return valueInPound;
 }
-
 // this is my function that sums two numbers
 const sum = (a,b) => {
     return a + b
 }
-
 // just a console log for ourselves.
 console.log(sum(7,3))
-
-// export the function to be used on other files 
+// export the function to be used on other files
+// (similar to the keyword `export` when using webpack)
 // we include fromEuroToDollar here as well because it needs to be exported
-module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound}
-
+module.exports = { sum, oneEuroIs, fromEuroToDollar, fromDollarToYen, fromYenToPound };
